8eb0192d1f31bf220b574d7d0d73bcca5d2fc178
Alex Bang <Alex.ZBP2456@GMAIL.COM>
2020-12-04 10:00:26 +0000

    Started Lab10

---
+++ Labs/lab10.py
@@ -0,0 +1,74 @@
+"""
+
+
+[10:07 AM] Bryan Duggan
+    well u should start by making the abstract class with the abstract methods
+​[10:08 AM] Bryan Duggan
+    and then make the fibonacchi class extend the abstract class
+​[10:08 AM] Bryan Duggan
+    and implement the abstract methods
+​[10:09 AM] C19409486 Alex Bang
+
+
+"""
+import abc
+class Maths:
+
+    @abc.abstractmethod
+    def __init__(self):
+        pass
+
+    @property
+    def user_input_property(self):
+        return
+
+    @abc.abstractmethod
+    def play_game(self):
+        print("Welcome to this game")
+
+class MathsGames(Maths):
+    # global variable
+
+    def __init__(self):
+        super().__init__()
+
+    def play_game(self):
+        guessMe = self.fibonacci()
+        print("What is the next number?")
+        userGuess = self.userGuess()
+        if userGuess == guessMe:
+            print("You win")
+        else:
+            print("You lose")
+
+    def fibonacci(self):
+        maxNumber = int(input("Enter the max number of fib: "))
+        previousNumber = 0
+        nextNumber = 1
+        fibArray = []
+        guessMe = 0
+
+        for i in range(0, maxNumber):
+            sum = previousNumber + nextNumber
+            previousNumber = nextNumber
+            nextNumber = sum
+            fibArray.append(sum)
+            guessMe = previousNumber + nextNumber
+
+        print(fibArray)
+        return guessMe
+
+
+    def userGuess(self):
+        userGuess = input("Your ans is: ")
+        return userGuess
+
+
+mg = MathsGames()
+mg.play_game()
+
+
+
+
+
+
--- Labs/lab_registration_record9.py
+++ Labs/lab_registration_record9.py
@@ -12,7 +12,10 @@ class Student:
 
     def __init__(self, study_type, f_name, l_name):
         # YOUR CODE GOES HERE
-        pass
+        self.study_type = study_type
+        self.f_name = f_name
+        self.l_name = l_name
+
 
     # YOUR CODE GOES HERE
 
@@ -24,9 +27,29 @@ class RegistrationData:
     """
     def __init__(self, address, registration_fee, study_type, f_name, l_name, s_id="NA"):
         # YOUR CODE GOES HERE
-        pass
+        self.address = address,
+        self.registration_fee = registration_fee,
+        self.study_type = study_type,
+        self.f_name = f_name,
+        self.l_name = l_name,
+        self.s_id = s_id
 
     # YOUR CODE GOES HERE
+    def get_address(new_address):
+        address = new_address
+
+    def set_address(self):
+        self.address = value
+
+    def get_registration_fee(registration_fee):
+        self.registration_fee = registration_fee
+
+    def set_registration_fee(self):
+        self.registration_fee = values
+
+    def get_study_type(self):
+        self.study_type
+
 
 
 
