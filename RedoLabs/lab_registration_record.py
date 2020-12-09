33cc838f204ab9a5d1cb185abe9e3e5adbf4cc90
Alex Bang <Alex.ZBP2456@GMAIL.COM>
2020-12-09 14:17:06 +0000

    lab 9 redo1

--- RedoLabs/lab_registration_record.py
+++ RedoLabs/lab_registration_record.py
@@ -10,11 +10,43 @@ class Student:
     INSERT YOUR DOCSTRING INFORMATION HERE
     """
 
+    STUDY_TYPE =  ("POSTGRADUATE", "UNDERGRADUATE")
+
     def __init__(self, study_type, f_name, l_name):
         # YOUR CODE GOES HERE
-        pass
+        self.__study_type = study_type
+        self.__f_name = f_name
+        self.__l_name = l_name
+        self.__courses = []
 
     # YOUR CODE GOES HERE
+    @property
+    def study_type(self):
+        return self.__study_type
+
+    @study_type.setter
+    def study_type(self, value):
+        self.__study_type = value
+
+    @property
+    def full_name(self):
+        return self.__f_name, self.__l_name
+
+    @full_name.setter
+    def full_name(self, value):
+        if type(value) != list:
+            print("AHHHHHHHHHHHHHHHHH")
+
+        self.__f_name = value[0]
+        self.__l_name = value[1]
+
+    # def name(self):
+    #     return {}{}.self
+
+
+    def __str__(self):
+        return f"{self.full_name} {self.study_type} {self.courses}"
+
 
 
 
@@ -23,10 +55,14 @@ class RegistrationData:
     INSERT YOUR DOCSTRING INFORMATION HERE
     """
     def __init__(self, address, registration_fee, study_type, f_name, l_name, s_id="NA"):
-        # YOUR CODE GOES HERE
-        pass
+        self.__address = address
+        self.__registration_fee = registration_fee
+        self.student_object = Student(study_type, f_name, l_name)
+        self.__s_id = s_id
 
     # YOUR CODE GOES HERE
+
+    def display_student_data(self):
 
 
 
